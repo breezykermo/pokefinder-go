69a62a48431820d9cb29fcef0faf4098d4e6eaff
Lachlan Kermode <lachlankermode@live.com>
2016-07-16 13:31:06 -0700

    Passing test suite

--- tests/reducers/pokemon.spec.js
+++ tests/reducers/pokemon.spec.js
@@ -1,20 +1,10 @@
 import test from 'ava'
 import reducer from '../../src/reducers/pokemon'
+import defaultState from '../../src/reducers/pokemon/defaultState'
 import * as actions from '../../src/reducers/pokemon/actions'
 
 // reducer testing
-test('change value from a fresh state', t => {
-  const state = reducer(undefined, actions.updateDemo('breeze'))
-  t.deepEqual(state.toJS(), { demo: 'breeze' })
-})
-
-test('change value from a previous state', t => {
-  const state = reducer(fromJS({ demo: 'breeze' }), actions.updateDemo('breezier'))
-  t.deepEqual(state.toJS(), { demo: 'breezier' })
-})
-
-test('play nicely with other reducers', t => {
-  const previousState = fromJS({ demo: 'past' })
-  const state = reducer(previousState, { type: 'NONE' })
-  t.is(state, previousState)
+test('generates default state from undefined', t => {
+  const state = reducer(undefined, { type: 'NONE' })
+  t.is(state, defaultState)
 })
