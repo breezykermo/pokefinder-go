1fde9f3772cf8ca821b4fa06e084e0d45f104466
Andrew Hunt <andrew.hunt@airbnb.com>
2016-07-15 22:55:04 -0700

    yyyyyy

--- src/components/Topbar/Topbar.styles.js
+++ src/components/Topbar/Topbar.styles.js
@@ -6,11 +6,8 @@ export default {
   container: {
     position: 'absolute',
     top: 0,
-<<<<<<< 9bd5abb05f31c96d3989089e2fcd1bc2471c42be
     height: 70,
-=======
     height: 100,
->>>>>>> [pokedex] go
     width: screenWidth,
     backgroundColor: '#ea2225',
     justifyContent: 'center',
