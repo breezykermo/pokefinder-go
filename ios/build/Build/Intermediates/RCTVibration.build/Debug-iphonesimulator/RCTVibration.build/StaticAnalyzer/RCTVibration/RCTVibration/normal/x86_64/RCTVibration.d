d91174ce1f1d1cd6a8f7099fc02254ac8ffbb090
Lachlan Kermode <lachlankermode@live.com>
2016-07-16 22:38:54 -0700

    Add background geolocation in iOS

--- ios/build/Build/Intermediates/RCTVibration.build/Debug-iphonesimulator/RCTVibration.build/StaticAnalyzer/RCTVibration/RCTVibration/normal/x86_64/RCTVibration.d
+++ ios/build/Build/Intermediates/RCTVibration.build/Debug-iphonesimulator/RCTVibration.build/StaticAnalyzer/RCTVibration/RCTVibration/normal/x86_64/RCTVibration.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/andrew_hunt/Documents/hack/atandt/pokedex-go/node_modules/react-native/Libraries/Vibration/RCTVibration.m \
-  /Users/andrew_hunt/Documents/hack/atandt/pokedex-go/node_modules/react-native/Libraries/Vibration/RCTVibration.h \
+  /Users/lachlankermode/hackathon/pokedex-go/node_modules/react-native/Libraries/Vibration/RCTVibration.m \
+  /Users/lachlankermode/hackathon/pokedex-go/node_modules/react-native/Libraries/Vibration/RCTVibration.h \
   ../../React/Base/RCTBridgeModule.h ../../React/Base/RCTDefines.h
