ef298dbd960a591d0eff9df12024647a23ec9f4d
Lachlan Kermode <lachlankermode@live.com>
2016-07-17 18:01:56 -0700

    Update UI (via inefficient complete re-render)

--- src/reducers/user/index.js
+++ src/reducers/user/index.js
@@ -1,7 +1,20 @@
 import defaultState from './defaultState'
+import { TOGGLE_POKEMON } from './actions'
 
 export default (state = defaultState, action) => {
   switch (action.type) {
+    case TOGGLE_POKEMON: {
+      console.log('Toggling pokemon ')
+      const watchlist = state.get('watchlist')
+      let newState
+      const ind = watchlist.toJS().indexOf(action.id)
+      if (ind !== -1) {
+        newState = state.set('watchlist', watchlist.delete(ind))
+      } else {
+        newState = state.set('watchlist', watchlist.push(action.id))
+      }
+      return newState
+    }
 
     default:
       return state
