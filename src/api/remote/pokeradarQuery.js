5aca920f8e29a19280801bbe386bb5afef7f4cfb
Lachlan Kermode <lachlankermode@live.com>
2016-07-19 17:11:22 -0700

    Unsuccessful attempts to return on pokeradar API

--- src/api/remote/pokeradarQuery.js
+++ src/api/remote/pokeradarQuery.js
@@ -11,7 +11,9 @@ export default (x, y) => {
   return Promise.resolve()
     .then(() => (
       // NB: hardcode a hack ID, seems to work better
-      fetch(`${POKERADAR_API}?deviceId=jo14hqg7xn18gm80avkk2i8tsva7v8m6&minLatitude=${swLoc.latitude()}&minLongitude=${swLoc.longitude()}&maxLatitude=${neLoc.latitude()}&maxLongitude=${neLoc.longitude()}&pokemonId=0`)
+      fetch(`${POKERADAR_API}?minLatitude=${swLoc.latitude()}&minLongitude=${swLoc.longitude()}&maxLatitude=${neLoc.latitude()}&maxLongitude=${neLoc.longitude()}&pokemonId=0`, {
+        method: 'GET',
+      })
     )).then(response => response.json())
     .then(pokeradarData => pokeradarData)
 }
